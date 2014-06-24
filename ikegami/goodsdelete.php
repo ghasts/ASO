ad40e136f9570e5039d0fb46ed1a6170bc178aeb
mikegami <1201502@st.asojuku.ac.jp>
2014-06-24 12:06:38 +0900

    no message

---
+++ ikegami/goodsdelete.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>PHP TEST</title>
+</head>
+<body>
+
+<?php
+$goodscode = $_POST["goods"];
+
+
+require_once 'DB.php';
+
+
+$dsn = 'mysqli://adios:lovelive@172.20.17.216/adios';
+
+$db = DB::connect($dsn);
+if (PEAR::isError($db)) {
+    die($db->getMessage());
+}
+
+
+
+
+
+
+$sql = "update goods set dispflag = '0' where goodscode = $goodscode;";
+$res =& $db->query($sql);
+if (PEAR::isError($res)) {
+    die($res->getMessage());
+}
+
+$sql = "select goods,size from goods where goodscode = $goodscode;";
+$res =& $db->query($sql);
+if (PEAR::isError($res)) {
+    die($res->getMessage());
+}
+while ($row =& $res->fetchRow(DB_FETCHMODE_OBJECT)){
+echo "'$row->goods' '$row->size'の商品の情報の非表示化に成功しました。";
+
+}
+
+
+$db->disconnect();
+
+?>
+
+</body>
+</html>
+
+
+
+
---
+++ ikegami/monthuriage.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>PHP TEST</title>
+</head>
+<body>
+
+<?php
+$year = $_POST["year"];
+$month = $_POST["month"];
+
+
+require_once 'DB.php';
+
+
+$dsn = 'mysqli://adios:lovelive@172.20.17.216/adios';
+
+$db = DB::connect($dsn);
+if (PEAR::isError($db)) {
+    die($db->getMessage());
+}
+
+
+$sql = "select sum(volume*price) as sum from detail join goods on detail.goodscode=goods.goodscode join stock on goods.stockcode=stock.stockcode join buy on detail.ordercode=buy.ordercode where YEAR(buy.date)=$year and MONTH(buy.date)=$month;";
+$res =& $db->query($sql);
+if (PEAR::isError($res)) {
+    die($res->getMessage());
+}
+
+echo '<h3>',$year,'年 ',$month,'月の売上は';
+
+while ($row =& $res->fetchRow(DB_FETCHMODE_OBJECT)){
+    print($row->sum);
+}
+echo '円です。</h3>';
+
+echo '<br><br><br><br><br><br>';
+$sql = "select goods.goods, sum(volume*price) as sum from detail join goods on detail.goodscode=goods.goodscode join stock on goods.stockcode=stock.stockcode join buy on detail.ordercode=buy.ordercode where YEAR(buy.date)=$year and MONTH(buy.date)=$month GROUP BY goods.goods;";
+$res =& $db->query($sql);
+if (PEAR::isError($res)) {
+    die($res->getMessage());
+}
+
+
+
+echo '<table border=4>';
+echo '<tr bgcolor="#cccccc">';
+echo '<th colspan=2>';
+echo $year,'年 ',$month,'月の商品別売上';
+echo '</th></tr>';
+
+while ($row =& $res->fetchRow(DB_FETCHMODE_OBJECT)){
+	echo '<tr><td>';
+	echo($row->goods);
+	echo '</td><td>';
+    echo($row->sum);
+	echo '円</td></tr>';
+}
+
+echo '</table>';
+
+
+$db->disconnect();
+
+?>
+
+</body>
+</html>
+
+
+
+
